f8fc4641234683f1c30ae08ab136c71d078599a0
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-21 10:54:42 +0000

    Fixed small issue with install requirements.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dce/trunk@58270 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '4.5.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 			'extbase' => '1.3.0-0.0.0',
 			'fluid' => '1.3.0-0.0.0',
 		),
