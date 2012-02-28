9e160c91bd35bea050d8774dec5ebd2738344394
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-28 09:55:03 +0000

    Changed extension description.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dce/trunk@58683 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Dynamic Content Elements (DCE)',
-	'description' => 'Creates dynamic content elements. It is an alternative to flexible content elements (FCE) but without need of TemplaVoila (TV).',
+	'description' => 'Creates dynamic content elements. It is an alternative to flexible content elements (FCE) but without need of TemplaVoila (TV). Based on Extbase and Fluid.',
 	'category' => 'frontend',
 	'author' => 'Armin Ruediger Vieweg',
 	'author_email' => 'armin@v.ieweg.de',
