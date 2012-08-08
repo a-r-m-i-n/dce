f8082bdeb6583a5dc38a37793f9641419492798b
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-08 22:44:55 +0000

    Add GP viewhelper, which gets GET or POST variables by identifier. Should be used only for usage in if conditions!!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dce/trunk@65282 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/ViewHelpers/GPViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Armin Ruediger Vieweg <armin@v.ieweg.de>
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * GP viewhelper which returns get or post variables using _GP method of t3lib_div.
+ * Never use this viewhelper for direct output!! This would provoke XSS (Cross site scripting).
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class Tx_Dce_ViewHelpers_GPViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * Returns get or post variable by given subject
+	 *
+	 * @param string $subject Name of variable to get
+	 * @return string Value of requested get or post variable. Don't output it directly! ( XSS risk)
+	 */
+	public function render($subject = NULL) {
+		if ($subject === NULL) {
+			$subject = $this->renderChildren();
+		}
+
+		return t3lib_div::_GP($subject);
+	}
+}
+
+?>
