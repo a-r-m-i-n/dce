dd224eb8a8e6f63dcc6848d02065d957e8abf3f4
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-10 19:36:54 +0000

    Add ContextHelpDataProvider to autoloader to prevent warning when saving DCE in TYPO3 6.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dce/trunk@77460 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,6 @@
 <?php
 $extensionPath = t3lib_extMgm::extPath('dce');
-return array(
+$autoloadClasses = array(
 	'tx_dce_controller_dcemodulecontroller' => $extensionPath . 'Classes/Controller/DceModuleController.php',
 	'tx_dce_cache' => $extensionPath . 'Classes/Cache.php',
 	'tx_dce_utility_fluidtemplate' => $extensionPath . 'Classes/Utility/FluidTemplate.php',
@@ -10,5 +10,14 @@ return array(
 	'tx_dce_viewhelpers_format_wrapwithcurlybracesviewhelper' => $extensionPath . 'Classes/ViewHelpers/Format/WrapWithCurlyBracesViewHelper.php',
 	'tx_dce_viewhelpers_include_resourceviewhelper' => $extensionPath . 'Classes/ViewHelpers/Include/ResourceViewHelper.php',
 	'tx_dce_domain_repository_dcerepository' => $extensionPath . 'Classes/Domain/Repository/DceRepository.php',
+	'ux_t3lib_transferdata' => $extensionPath . 'Classes/XClasses/class.ux_t3lib_transferdata.php'
 );
+
+$typo3Version = TYPO3_version;
+if (intval($typo3Version{0}) === 6) {
+	$autoloadClasses['TYPO3\\CMS\\ContextHelp\\ExtDirect\\ContextHelpDataProvider'] =
+		PATH_typo3 . 'sysext/context_help/Classes/ExtDirect/ContextHelpDataProvider.php';
+}
+
+return $autoloadClasses;
 ?>
