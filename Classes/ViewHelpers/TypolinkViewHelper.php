a8ee207f8c7ffcf6312b69b56c1c346e45d5dda0
Armin Vieweg <armin@v.ieweg.de>
2015-08-12 22:05:27 +0200

    [FEATURE] Add class, title and target attribute to Typolink ViewHelper
    Normally the informations for the link's class, title or target, is set by user in editor. But sometimes you want to overwrite these values.
    
    <f:typolink parameter="{field.typolinkParameterString}" class="myClass" title="Hi!">...</f:typolink.parameter>
    
    Resolves: #68654

--- Classes/ViewHelpers/TypolinkViewHelper.php
+++ Classes/ViewHelpers/TypolinkViewHelper.php
@@ -1,11 +1,12 @@
 <?php
 namespace ArminVieweg\Dce\ViewHelpers;
 
-    /*  | This extension is part of the TYPO3 project. The TYPO3 project is
-     *  | free software and is licensed under GNU General Public License.
-     *  |
-     *  | (c) 2012-2015 Armin Ruediger Vieweg <armin@v.ieweg.de>
-     */
+/*  | This extension is part of the TYPO3 project. The TYPO3 project is
+ *  | free software and is licensed under GNU General Public License.
+ *  |
+ *  | (c) 2012-2015 Armin Ruediger Vieweg <armin@v.ieweg.de>
+ */
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * This view helper handles parameter strings using typolink function of TYPO3.
@@ -21,16 +22,34 @@ class TypolinkViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHe
      * @param string $parameter Parameter string, which can be handled by
      *                          typolink functionality
      * @param string $subject Link text
+     * @param string $class If set, overrides given class in parameter string.
+     * @param string $target If set, overrides given target in parameter string.
+     * @param string $title If set, overrides given title in parameter string.
      * @return string Rendered HTML <a>-tag
      */
-    public function render($parameter, $subject = null)
+    public function render($parameter, $subject = null, $class = null, $target = null, $title = null)
     {
         if ($subject === null) {
             $subject = $this->renderChildren();
         }
 
         /** @var $cObj \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer */
-        $cObj = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
+        $cObj = GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
+        if (!is_null($class) || !is_null($target) || !is_null($title)) {
+            /** @var \ArminVieweg\Dce\Utility\TypoLinkCodecService $TypoLinkCodecService */
+            $typoLinkCodecService = GeneralUtility::makeInstance('ArminVieweg\Dce\Utility\TypoLinkCodecService');
+            $typolinkParameterParts = $typoLinkCodecService->decode($parameter);
+            if (!is_null($class)) {
+                $typolinkParameterParts['class'] = $class;
+            }
+            if (!is_null($target)) {
+                $typolinkParameterParts['target'] = $target;
+            }
+            if (!is_null($title)) {
+                $typolinkParameterParts['title'] = $title;
+            }
+            $parameter = $typoLinkCodecService->encode($typolinkParameterParts);
+        }
         return $cObj->getTypoLink($subject, $parameter);
     }
 }
