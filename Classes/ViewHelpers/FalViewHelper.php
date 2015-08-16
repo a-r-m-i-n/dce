789a7692e5e10705ce2c2af18b11ed775a1032bf
Armin Vieweg <armin@v.ieweg.de>
2015-08-16 19:09:39 +0200

    [BUGFIX] DCE's FAL ViewHelper can now handle translated media
    Resolves: #69026

--- Classes/ViewHelpers/FalViewHelper.php
+++ Classes/ViewHelpers/FalViewHelper.php
@@ -24,12 +24,17 @@ class FalViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
      * @param string $field Name of field in DCE
      * @param array $contentObject Content object data array, which is stored
      *                             in {contentObject} in dce template.
-     *
+     * @param bool $localizeUid
      * @return array|string String or array with found media
      */
-    public function render($field, array $contentObject)
+    public function render($field, array $contentObject, $localizeUid = true)
     {
         $contentObjectUid = intval($contentObject['uid']);
+        if ($localizeUid) {
+            $contentObjectUid = intval(
+                $contentObject['_LOCALIZED_UID'] != null ? $contentObject['_LOCALIZED_UID'] : $contentObject['uid']
+            );
+        }
 
         /** @var \TYPO3\CMS\Frontend\Page\PageRepository $pageRepository */
         $pageRepository = GeneralUtility::makeInstance('TYPO3\CMS\Frontend\Page\PageRepository');
