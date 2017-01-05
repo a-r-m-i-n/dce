d4bfba6955dd0aff9b00d78916ce85c3c7b9d0e3
Armin Vieweg <armin@v.ieweg.de>
2017-01-05 11:43:52 +0100

    [TASK] Update text of licence notice
    Licence of DCE is still GPL, no change here.

--- Classes/UserFunction/CustomFieldValidation/LowerCamelCaseValidator.php
+++ Classes/UserFunction/CustomFieldValidation/LowerCamelCaseValidator.php
@@ -1,8 +1,8 @@
 <?php
 namespace ArminVieweg\Dce\UserFunction\CustomFieldValidation;
 
-/*  | This extension is part of the TYPO3 project. The TYPO3 project is
- *  | free software and is licensed under GNU General Public License.
+/*  | This extension is made for TYPO3 CMS and is licensed
+ *  | under GNU General Public License.
  *  |
  *  | (c) 2012-2017 Armin Ruediger Vieweg <armin@v.ieweg.de>
  */
