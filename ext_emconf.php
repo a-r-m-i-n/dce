ec1f648c556f12aef62fcc9bad7cd7217a778dca
Armin Vieweg <armin@v.ieweg.de>
2017-04-04 22:24:32 +0200

    [TASK] Update Extension's Description

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,10 +6,10 @@
  *  | (c) 2012-2017 Armin Ruediger Vieweg <armin@v.ieweg.de>
  */
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Dynamic Content Elements (DCE)',
     'description' => 'Best flexform based content elements since 2012. With TCA mapping feature, simple backend view ' .
-        'and many more features which makes it easy to create own content element types.',
+        'and much more features which makes it super easy to create own content element types.',
     'category' => 'Backend',
     'shy' => 0,
     'version' => '1.4.2',
@@ -29,12 +29,12 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'CGLcompliance' => '',
     'CGLcompliance_note' => '',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.0-8.9.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    'suggests' => array(),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'suggests' => [],
+];
