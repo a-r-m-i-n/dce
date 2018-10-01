33a375e6141881ba9fd41a35d46c6f68a7db30d5
Armin Vieweg <armin@v.ieweg.de>
2018-10-01 20:00:23 +0200

    [TASK] Clean up Code Snippets
    Removed all 7.6 snippets.
    Also removed "checkSnippetNameForVersionConstraintAndCurrentVersion"
    functionality. All xml files will get displayed always, in field
    configuration dropdown.

--- Classes/UserFunction/UserFields/CodemirrorField.php
+++ Classes/UserFunction/UserFields/CodemirrorField.php
@@ -90,7 +90,7 @@ class CodemirrorField
                 foreach ($rows as $row) {
                     if ($row['type'] === '2') {
                         $sectionFields = $db->exec_SELECTgetRows(
-                            'title, variable',
+                            '*',
                             'tx_dce_domain_model_dcefield',
                             'deleted=0 AND parent_field=' . $row['uid'],
                             '',
@@ -119,9 +119,7 @@ class CodemirrorField
             $files = [];
             foreach (GeneralUtility::getFilesInDir($path . $key) as $file) {
                 $filename = preg_replace('/(.*)\.xml/i', '$1', $file);
-                if ($this->checkSnippetNameForVersionConstraintAndCurrentVersion($filename)) {
-                    $files[$filename] = file_get_contents($path . $key . '/' . $file);
-                }
+                $files[$filename] = file_get_contents($path . $key . '/' . $file);
             }
             $keyNoNumber = preg_replace('/.*? (.*)/i', '$1', $key);
 
@@ -158,35 +156,13 @@ class CodemirrorField
     protected function getViewhelpers($path)
     {
         $files = GeneralUtility::getFilesInDir($path);
-
         $viewHelpers = [];
         foreach ($files as $file) {
             $name = preg_replace('/(.*)\.html/i', '$1', $file);
-            if ($this->checkSnippetNameForVersionConstraintAndCurrentVersion($name)) {
-                $value = file_get_contents($path . $file);
-                $viewHelpers[$name] = $value;
-            }
+            $value = file_get_contents($path . $file);
+            $viewHelpers[$name] = $value;
         }
         ksort($viewHelpers);
         return $viewHelpers;
-    }
-
-    /**
-     * Checks if given snippet name contains a version number.
-     * If not this method will return true.
-     *
-     * If it contains a version number (e.g. "Cool Snippet (7.6)") it is checking this version number
-     * against GeneralUtility::compat_version and return its return value.
-     *
-     * So if version number in snippet name is 8.7 but the current TYPO3 version is 7.6, the method
-     * will return false. In case it's TYPO3 8.7 it would return true.
-     *
-     * @param string $snippetName Name of snippet which may contain TYPO3 version number (in braces)
-     * @return bool True if no version number in snippet name or given version number fits current TYPO3 version
-     */
-    protected function checkSnippetNameForVersionConstraintAndCurrentVersion($snippetName)
-    {
-        preg_match('/\((\d\.\d)\)/i', $snippetName, $matches);
-        return empty($matches) || !empty($matches) && TYPO3_branch === $matches[1];
     }
 }
