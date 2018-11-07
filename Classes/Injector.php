fec6ca4611c4726ebe744b3b29ff383fff0ba2a2
Andy Kucera <andy@dercoder.com>
2018-11-07 13:07:15 +0000

    [BUGFIX] Fix label in category tab

--- Classes/Injector.php
+++ Classes/Injector.php
@@ -78,8 +78,7 @@ class Injector
                 ? '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.media,assets,'
                 : '';
             $showCategoryTabCode = $dce['show_category_tab']
-                ? '--div--;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category,' .
-                  'categories,'
+                ? '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,categories,'
                 : '';
 
             $GLOBALS['TCA']['tt_content']['types']['dce_dceuid' . $dce['uid'] . '']['showitem'] =
