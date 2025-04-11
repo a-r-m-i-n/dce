3ae64a95b3800c43e5e0a9fbc2f0cefcb514a4e3
Armin Vieweg <armin@v.ieweg.de>
2025-04-11 13:25:29 +0200

    [TASK] Fix ES6 configuration for DCE javascript
    Removed "AfterFormEnginePageInitializedEventListener", in favour
    of correct JavaScriptModules configuration.
    
    See https://forge.typo3.org/issues/102111

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         '@t3/dce/code-editor' => 'EXT:dce/Resources/Public/JavaScript/DceCodeEditor.js',
         '@t3/dce/dce-edit-button' => 'EXT:dce/Resources/Public/JavaScript/DceEditButton.js',
