7a3429a17d59b09391ca08235ff032ab21e244e3
Armin Vieweg <armin@v.ieweg.de>
2026-08-09 17:57:04 +0200

    [BUGFIX] Handle missing DCE edit button

--- Resources/Public/JavaScript/DceEditButton.js
+++ Resources/Public/JavaScript/DceEditButton.js
@@ -1,8 +1,10 @@
 const dceEditButton = document.querySelector('a[data-dce-edit-url]');
 
-dceEditButton.addEventListener('click', function (event) {
-    event.preventDefault();
+if (dceEditButton) {
+    dceEditButton.addEventListener('click', function (event) {
+        event.preventDefault();
 
-    const editUrl = dceEditButton.dataset.dceEditUrl;
-    window.open(editUrl, 'editDcePopup', 'height=768,width=1024,status=0,menubar=0,scrollbars=1')
-});
+        const editUrl = dceEditButton.dataset.dceEditUrl;
+        window.open(editUrl, 'editDcePopup', 'height=768,width=1024,status=0,menubar=0,scrollbars=1')
+    });
+}
